12213cbaeb9fcc8fa5176877629e928351c8d735
IvanVaverchak <ivanvaverchak@2000gmail.com>
2024-03-13 00:26:45 +0200

    modify nd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Wrapper apiKey={process.env.REACT_APP_GOOGLE_API_KEY!} render={render}>
+      <Wrapper apiKey={process.env.REACT_APP_GOOGLE_API_KEY || ''} render={render}>
         <MapComponent />
       </Wrapper>
     </div>
