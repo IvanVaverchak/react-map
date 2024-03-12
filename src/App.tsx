44b00eb92e9a1c3e92bd519659d5e125ce30d55c
IvanVaverchak <ivanvaverchak@2000gmail.com>
2024-03-13 00:16:19 +0200

    modify nd

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import './App.css';
 import { Wrapper, Status } from '@googlemaps/react-wrapper';
 import MapComponent from './Map/MyMap';
 
-const render = (status: Status) => (<h1>{status}</h1>);
 
 function App() {
+  const render = (status: Status) => (<h1>{status}</h1>);
+
   return (
     <div className="App">
       <Wrapper apiKey={process.env.REACT_APP_GOOGLE_API_KEY!} render={render}>
